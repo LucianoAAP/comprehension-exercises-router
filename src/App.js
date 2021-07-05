2588ba24ff01915220d9c5055add0d3983d3cebf
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-07-05 20:35:59 -0300

    ex 10 conteudo

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={ Home } />
-        <Route path="/about" component={ About } />
-        <Route path="/users/:id" render={ (props) => <Users { ...props } greetingsMessage={ 'Good Morning' } /> } />
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route path="/about" component={ About } />
+          <Route path="/users/:id" render={ (props) => <Users { ...props } greetingsMessage={ 'Good Morning' } /> } />
+        </Switch>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/users">Users</Link>
