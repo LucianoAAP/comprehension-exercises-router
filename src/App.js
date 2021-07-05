89a330b7b22f413b15bcecba7b362ec75924acf6
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-07-05 19:56:48 -0300

    ex 6 conteudo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -11,6 +11,9 @@ class App extends Component {
         <Route exact path="/" component={ Home } />
         <Route path="/about" component={ About } />
         <Route path="/users" component={ Users } />
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
       </BrowserRouter>
     );
   }
